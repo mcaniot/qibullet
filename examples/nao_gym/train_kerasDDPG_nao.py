c60310a98f17ad4ec93b1b8f976e17dc35b7b1a1
mcaniot <mcaniot@aldebaran.com>
2019-06-13 16:53:16 +0200

    fix Pep8 format Signed-off-by: mcaniot <mcaniot@aldebaran.com>

--- examples/nao_gym/train_kerasDDPG_nao.py
+++ examples/nao_gym/train_kerasDDPG_nao.py
@@ -14,7 +14,8 @@ opts.env = "NaoBulletEnv"
 opts.train_for = 10000000
 opts.test_for = 100
 datenow = '{:%Y%m%d%H%M%S}'.format(datetime.datetime.now())
-opts.load_file = "checkpoints/%s-%s-%s.h5" % (opts.agent, opts.env, "test-3-v2")
+opts.load_file = "checkpoints/%s-%s-%s.h5" % (opts.agent, opts.env,
+                                              "test-3-v2")
 opts.save_file = "checkpoints/%s-%s-test-4-v2.h5"\
     % (opts.agent, opts.env)
 
