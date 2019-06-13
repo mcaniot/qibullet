a2542a218261ac29d96b13302876deb692135bb8
mcaniot <mcaniot@aldebaran.com>
2019-06-13 18:00:45 +0200

    pep8 + add agent + add NaoEnv Signed-off-by: mcaniot <mcaniot@aldebaran.com>

--- examples/nao_gym/test_kerasDDPG_nao.py
+++ examples/nao_gym/test_kerasDDPG_nao.py
@@ -13,7 +13,8 @@ opts.env = "NaoBulletEnv"
 opts.train_for = 0
 opts.test_for = 100
 datenow = '{:%Y%m%d%H%M%S}'.format(datetime.datetime.now())
-opts.load_file = "checkpoints/%s-%s-%s.h5" % (opts.agent, opts.env, "test-3-v2")
+opts.load_file = "checkpoints/%s-%s-%s.h5" % (opts.agent, opts.env,
+                                              "test-3-v2")
 
 print("\n OPTS", opts)
 trainer.setup_exercise(opts)
