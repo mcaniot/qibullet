1899bbed9f99c9484ebddf653bbc4b2a7d68d8e4
mcaniot <mcaniot@aldebaran.com>
2019-06-13 16:06:29 +0200

    add agent and code for training with DDPG Signed-off-by: mcaniot <mcaniot@aldebaran.com>

--- examples/nao_gym/agents/__init__.py
+++ examples/nao_gym/agents/__init__.py
@@ -1,36 +1,35 @@
-# noqa: F401
-# flake8: noqa
 # ---- register agents ----------
-from agents import agent_register
-from agents.agents_kerasrl import KerasCEMAgent, KerasDDPGAgent,\
-                           KerasDDQNAgent, KerasDQNAgent, KerasNAFAgent
+# import agent_register
 
-agent_register.register(
-    id='BaselinesDQNAgent-v0',
-    entry_point='agents_baselines:BaselinesDQNAgent'
-)
+# agent_register.register(
+# 	id='BaselinesDQNAgent-v0',
+# 	entry_point='agents_baselines:BaselinesDQNAgent'
+# )
 
-agent_register.register(
-    id='KerasCEMAgent-v0',
-    entry_point='agents.agents_kerasrl:KerasCEMAgent'
-)
-
-agent_register.register(
-    id='KerasDDPGAgent-v0',
-    entry_point='agents.agents_kerasrl:KerasDDPGAgent'
-)
-
-agent_register.register(
-    id='KerasDDQNAgent-v0',
-    entry_point='agents.agents_kerasrl:KerasDDQNAgent'
-)
-
-agent_register.register(
-    id='KerasDQNAgent-v0',
-    entry_point='agents.agents_kerasrl:KerasDQNAgent'
-)
-
-agent_register.register(
-    id='KerasNAFAgent-v0',
-    entry_point='agents.agents_kerasrl:KerasNAFAgent'
-)
+# agent_register.register(
+# 	id='KerasCEMAgent-v0',
+# 	entry_point='pybullet_envs.agents.agents_kerasrl:KerasCEMAgent'
+# )
+#
+# agent_register.register(
+# 	id='KerasDDPGAgent-v0',
+# 	entry_point='pybullet_envs.agents.agents_kerasrl:KerasDDPGAgent'
+# )
+#
+# agent_register.register(
+# 	id='KerasDDQNAgent-v0',
+# 	entry_point='pybullet_envs.agents.agents_kerasrl:KerasDDQNAgent'
+# )
+#
+# agent_register.register(
+# 	id='KerasDQNAgent-v0',
+# 	entry_point='pybullet_envs.agents.agents_kerasrl:KerasDQNAgent'
+# )
+#
+# agent_register.register(
+# 	id='KerasNAFAgent-v0',
+# 	entry_point='pybullet_envs.agents.agents_kerasrl:KerasNAFAgent'
+# )
+#
+# # from agents_baselines import BaselinesDQNAgent
+# from agents_kerasrl import KerasCEMAgent, KerasDDPGAgent, KerasDDQNAgent, KerasDQNAgent, KerasNAFAgent
--- examples/nao_gym/test_kerasDDPG_nao.py
+++ examples/nao_gym/test_kerasDDPG_nao.py
@@ -1,3 +1,4 @@
+import datetime
 import argparse
 import Trainer
 
@@ -11,7 +12,8 @@ opts.agent = "KerasDDPGAgent-v0"
 opts.env = "NaoBulletEnv"
 opts.train_for = 0
 opts.test_for = 100
-opts.load_file = "checkpoints/%s-%s.h5" % (opts.agent, opts.env)
+datenow = '{:%Y%m%d%H%M%S}'.format(datetime.datetime.now())
+opts.load_file = "checkpoints/%s-%s-%s.h5" % (opts.agent, opts.env, "test-3-v2")
 
 print("\n OPTS", opts)
 trainer.setup_exercise(opts)
