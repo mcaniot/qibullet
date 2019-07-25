8d5f297edb21858f6a46440e57dbe913bd1b55b3
mcaniot <mcaniot@aldebaran.com>
2019-07-25 09:30:26 +0200

    adapt env for PPO and set velocity Signed-off-by: mcaniot <mcaniot@aldebaran.com>

--- examples/nao_gym/train_baselinesPPO_nao.py
+++ examples/nao_gym/train_baselinesPPO_nao.py
@@ -1,63 +1,91 @@
 #!/usr/bin/env python3
+import os
 import sys
-try:
-    sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
-except Exception:
-    pass
-
+sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+from baselines.common import tf_util as U
+from baselines import logger
 import NaoEnv
+import gym
 import numpy as np
 import time
 from datetime import datetime
-
-from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines import PPO2
 
 ENV_ID = 'NaoBulletEnv'
 
 
 def train(num_timesteps, seed, model_path=None):
+    from baselines.ppo1 import mlp_policy, pposgd_simple
+    U.make_session(num_cpu=1).__enter__()
 
-    env = NaoEnv.NaoEnv(gui=True)
-    env = DummyVecEnv([lambda: env])
-    model = PPO2(
-                 MlpPolicy,
-                 env,
-                 n_steps=4096,
-                 verbose=2,
-                 tensorboard_log="logs/PPO2Agent/" + datetime.now().strftime(
-                     "%Y%m%d-%H%M%S"))
-    i = 0
-    while i < num_timesteps:
-        if i != 0:
-            model.load(model_path)
-        model.learn(total_timesteps=int(1e6))
-        model.save(model_path)
-        i += int(1e6)
+    def policy_fn(name, ob_space, ac_space):
+        return mlp_policy.MlpPolicy(
+            name=name,
+            ob_space=ob_space, ac_space=ac_space,
+            hid_size=64, num_hid_layers=2)
+    env = getattr(NaoEnv, ENV_ID)(render=True)
+
+    # parameters below were the best found in a simple random search
+    # these are good enough to make humanoid walk, but whether those are
+    # an absolute best or not is not certain
+    env = RewScale(env, 0.1)
+    logger.log("NOTE: reward will be scaled by a factor"
+               " of 10  in logged stats."
+               " Check the monitor for unscaled reward.")
+    pi = pposgd_simple.learn(
+            env, policy_fn,
+            max_timesteps=num_timesteps,
+            timesteps_per_actorbatch=4096,
+            clip_param=0.1, entcoeff=0.0,
+            optim_epochs=10,
+            optim_stepsize=1e-4,
+            optim_batchsize=64,
+            gamma=0.99,
+            lam=0.95,
+            schedule='constant'
+            )
     env.close()
+    if model_path:
+        U.save_state(model_path)
+
+    return pi
 
 
-def visualize(name_model):
-    model = PPO2.load(name_model)
-    env = NaoEnv.NaoEnv(gui=True)
-    env = DummyVecEnv([lambda: env])
-    # Enjoy trained agent
-    obs = env.reset()
+class RewScale(gym.RewardWrapper):
+    def __init__(self, env, scale):
+        gym.RewardWrapper.__init__(self, env)
+        self.scale = scale
+
+    def reward(self, r):
+        return r * self.scale
+
+
+def visualize():
+    # construct the model object, load pre-trained model and render
+    seed = int(time.time())
+    pi = train(num_timesteps=1, seed=seed)
+    U.load_state("models/")
+    env = getattr(NaoEnv, ENV_ID)(render=True)
+
+    ob = env.reset()
     while True:
-        action, _states = model.predict(obs)
-        obs, rewards, dones, info = env.step(action)
+        action = pi.act(stochastic=False, ob=ob)[0]
+        ob, _, done, _ = env.step(action)
         env.render()
+        if done:
+            ob = env.reset()
 
 
 def main():
+    logger.configure(dir="logs/PPOAgent/" + datetime.now().strftime(
+                     "%Y%m%d-%H%M%S"),
+                     format_strs=['stdout', 'log', 'csv', 'tensorboard'])
+    logger.set_level(10)
     seed = int(time.time())
     np.random.seed(seed)
     # train the model
-    name_model = "models_nao/PPO2_200M"
-    train(num_timesteps=int(20e7), seed=seed,
-          model_path=name_model)
-    # visualize(name_model)
+    train(num_timesteps=int(1e7), seed=seed,
+          model_path="models/")
+    # visualize()
 
 if __name__ == '__main__':
     main()
