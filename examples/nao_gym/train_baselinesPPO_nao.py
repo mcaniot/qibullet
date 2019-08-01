2b23c202eb31574a52a66e63ad19e76ca3d2ccee
mcaniot <mcaniot@aldebaran.com>
2019-08-01 13:47:37 +0200

    change reward and reset, add nao_env in ppo train Signed-off-by: mcaniot <mcaniot@aldebaran.com>

--- examples/nao_gym/train_baselinesPPO_nao.py
+++ examples/nao_gym/train_baselinesPPO_nao.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+import os
+import sys
+sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 from baselines.common import tf_util as U
 from baselines import logger
 import NaoEnv
@@ -6,6 +9,7 @@ import gym
 import numpy as np
 import time
 from datetime import datetime
+from pybullet_envs.gym_locomotion_envs import HumanoidBulletEnv
 
 ENV_ID = 'NaoBulletEnv'
 
@@ -19,8 +23,9 @@ def train(num_timesteps, seed, model_path=None):
             name=name,
             ob_space=ob_space, ac_space=ac_space,
             hid_size=64, num_hid_layers=2)
-    env = getattr(NaoEnv, ENV_ID)(render=True)
-
+    env = NaoEnv.NaoEnv(gui=True)
+    # env = HumanoidBulletEnv()
+    env.render()
     # parameters below were the best found in a simple random search
     # these are good enough to make humanoid walk, but whether those are
     # an absolute best or not is not certain
@@ -56,18 +61,47 @@ class RewScale(gym.RewardWrapper):
         return r * self.scale
 
 
-def visualize():
+def visualize(name_model):
     # construct the model object, load pre-trained model and render
     seed = int(time.time())
-    pi = train(num_timesteps=1, seed=seed)
-    U.load_state("models/")
-    env = getattr(NaoEnv, ENV_ID)(render=True)
+    from baselines.ppo1 import mlp_policy, pposgd_simple
+    U.make_session(num_cpu=1).__enter__()
 
+    def policy_fn(name, ob_space, ac_space):
+        return mlp_policy.MlpPolicy(
+            name=name,
+            ob_space=ob_space, ac_space=ac_space,
+            hid_size=64, num_hid_layers=2)
+    env = getattr(NaoEnv, ENV_ID)(render=False)
+    # env = HumanoidBulletEnv()
+    env.render()
+    # parameters below were the best found in a simple random search
+    # these are good enough to make humanoid walk, but whether those are
+    # an absolute best or not is not certain
+    env = RewScale(env, 0.1)
+    logger.log("NOTE: reward will be scaled by a factor"
+               " of 10  in logged stats."
+               " Check the monitor for unscaled reward.")
+    pi = pposgd_simple.learn(
+            env, policy_fn,
+            max_timesteps=1,
+            timesteps_per_actorbatch=4096,
+            clip_param=0.1, entcoeff=0.0,
+            optim_epochs=10,
+            optim_stepsize=1e-4,
+            optim_batchsize=64,
+            gamma=0.99,
+            lam=0.95,
+            schedule='constant'
+            )
+    U.load_state(name_model)
     ob = env.reset()
+    print("ok")
     while True:
         action = pi.act(stochastic=False, ob=ob)[0]
         ob, _, done, _ = env.step(action)
         env.render()
+        # time.sleep(0.1)
         if done:
             ob = env.reset()
 
@@ -80,9 +114,10 @@ def main():
     seed = int(time.time())
     np.random.seed(seed)
     # train the model
+    name_model = "models_nao/"
     train(num_timesteps=int(1e7), seed=seed,
-          model_path="models/")
-    # visualize()
+          model_path=name_model)
+    # visualize(name_model)
 
 if __name__ == '__main__':
     main()
